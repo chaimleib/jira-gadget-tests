284b5b3730b1787875699f15a8e928e391b4c937
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-06-11 17:41:04 -0700

    made jira_config.rb also work as a shell script

--- jira_config.rb
+++ jira_config.rb
@@ -9,16 +9,17 @@ class JiraConfig
     @username = ''
     @password = ''
     @host = ''
+    @data = {}
     load_config
   end
   
   def load_config
     f = File.open @path
-    data = YAML.load f.read
+    @data = YAML.load f.read
     f.close
-    @username = data['username']
-    @password = data['password']
-    @host = data['host']
+    @username = @data['username']
+    @password = @data['password']
+    @host = @data['host']
     self
   end
   
@@ -29,4 +30,39 @@ class JiraConfig
   def login_string
     "#{@username}:#{@password}"
   end
+  
+  def shell_vars
+    lines = @data.collect { |k, v| "#{k}=#{v.inspect}" }
+    lines.join "\n"
+  end
+    
 end
+
+if __FILE__ == $0
+  def usage
+    puts "#{__FILE__} [commands ...]
+    
+    Commands:
+      username
+      password
+      host
+      shell_vars    - print bash script to set variables for the above
+      login_string  - 'username:password'
+      -h, --help    - print this help message
+      "
+  end
+  
+  cfg = JiraConfig.new
+  
+  if ARGV.empty?
+    puts cfg.shell_vars
+  elsif (ARGV & ['-h', '--help']).any?
+    usage
+  else
+    ARGV.each do |arg|
+      puts cfg.send arg
+    end
+  end
+  
+end
+
