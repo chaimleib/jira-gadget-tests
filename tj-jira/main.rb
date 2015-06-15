c6841fbfc7bc0186059018fb350eb01b55ccb58f
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-06-15 15:59:22 -0700

    fix: tj-jira username format, JiraTicket accessors

--- tj-jira/main.rb
+++ tj-jira/main.rb
@@ -7,7 +7,11 @@ require 'pp'
 require '../jira_config'
 
 class JiraConnection
-  EMAIL_RGX = /^[^@]+@[^@]+$/
+  ## Email username
+  #USER_RGX = /^[^@]+@[^@]+$/
+  ## Identifier username
+  USER_RGX = /^[-_a-zA-Z0-9\.]+$/
+  
   URI_RGX = /^https?:\/\/[-.\/a-zA-Z0-9]+$/
   
   def initialize(
@@ -34,16 +38,14 @@ class JiraConnection
       end
     end
 
-    raise "`#{@username}` is an invalid e-mail for JIRA" if @username !~ EMAIL_RGX
+    raise "`#{@username}` is an invalid e-mail for JIRA" if @username !~ USER_RGX
     raise "No password provided" if @password.empty?
     raise "`#{@host}` is an invalid host URI" if @host.empty? || @host !~ URI_RGX
-
-    puts @username
   end
 
   def prompt_login
     prompted = false
-    while @username !~ EMAIL_RGX
+    while @username !~ USER_RGX
       if not @username.empty?
         puts ">> `#{@username}` is not a valid e-mail!\n\n"
       elsif prompted
@@ -92,32 +94,27 @@ class JiraConnection
 
   def submit_search(jql='')
     return if jql.empty?
-
     jql = URI.escape jql
     # puts jql
-
     uri = URI.parse "#{@host}/rest/api/2/search?jql=#{jql}"
     puts uri
-
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     request = Net::HTTP::Get.new(uri.request_uri)
     request.basic_auth @username, @password
     request["Content-Type"] = "application/json"
-
     response = http.request(request)
     puts response.body
-
     raise "#{response.code}: #{response.message}" if response.code !~ /20[0-9]/
     data = JSON.parse(response.body)
   end
 
   def pull_tickets
-  #  jql = "sprint=483+and+remainingestimate>0+order+by+assignee"
+    #jql = "sprint=483+and+remainingestimate>0+order+by+assignee"
     #jql = "sprint=505+and+type='Technical task'"
     jql = "sprint=505"
-    data = submit_search(jql)
+    data = submit_search jql
 
     raise 'Search failed' unless data
 
@@ -126,7 +123,7 @@ class JiraConnection
     #puts JSON.pretty_generate(data)
 
     data["issues"].each do |issue|
-      get_ticket(issue["key"])
+      get_ticket issue["key"]
     end
 
     #puts @estimate
@@ -138,11 +135,9 @@ class JiraConnection
     #pp @estimate
 
     @jiraTickets.each do |key, jiraTicket|
-      print "#{key}:"
-      #jiraTicket1 = JiraTicket.new(jiraTicket)
-
-      p jiraTicket.createdDate
-      #print jiraTicket.status
+      puts "#{key}:#{jiraTicket.jiraId},#{jiraTicket.createdDate}"
+      print "  status:"
+      p jiraTicket.status
     end
   end
 
@@ -157,63 +152,62 @@ class JiraConnection
     #json_ext = ".json"
 
     #for issue in issue_keys
-      uri = URI.parse("#{@host}/rest/api/latest/issue/" + issue)
-      puts ''
-      puts uri
-      puts ''
+    uri = URI.parse "#{@host}/rest/api/latest/issue/#{issue}"
+    puts ''
+    puts uri
+    puts ''
 
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      request = Net::HTTP::Get.new(uri.request_uri)
-      request.basic_auth @username, @pwd
-      request["Content-Type"] = "application/json"
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(uri.request_uri)
+    request.basic_auth @username, @password
+    request["Content-Type"] = "application/json"
 
-      response = http.request(request)
+    response = http.request(request)
 
-      if response.code =~ /20[0-9]{1}/
-        data = JSON.parse(response.body)
-        fields = data.keys
-
-        #puts JSON.pretty_generate(data)
-        print data["key"] + ","
-        print data["fields"]["reporter"]["displayName"] + ","
-        print data["fields"]["assignee"]["displayName"] + ","
-        print  data["fields"]["status"]["name"] + ","
-
-        data["fields"]["components"].each do |key|
-          print key["name"] + ";"
-        end
-
-        if data["fields"]["summary"].include? "QE "
-
-          ticket1 =  JiraTicket.new issue, data["fields"]["summary"],data["fields"]["created"],data["fields"]["status"]
-          @jiraTickets[issue] = ticket1
-          puts "======" + issue + data["fields"]["summary"] + data["fields"]["created"] + data["fields"]["status"]
-        end
-
-        data["fields"]["subtasks"].each do |key|
-          print key["key"] + ";"
-        end
-        print ","
-
-        print data["fields"]["created"] + ","
-
-        print data["fields"]["timetracking"]["originalEstimate"].to_s + "," unless data["fields"]["timetracking"]["originalEstimateSeconds"] == nil
-        print data["fields"]["timetracking"]["remainingEstimate"].to_s + "," unless data["fields"]["timetracking"]["originalEstimateSeconds"] == nil
-        #print data["fields"]["timetracking"]["timeSpent"] + ";"
-        print data["fields"]["timetracking"]["originalEstimateSeconds"].to_s + "," unless data["fields"]["timetracking"]["originalEstimateSeconds"] == nil
-        print data["fields"]["timetracking"]["remainingEstimateSeconds"].to_s + "," unless data["fields"]["timetracking"]["originalEstimateSeconds"] == nil
-        print data["fields"]["timetracking"]["timeSpentSeconds"].to_s + "," unless data["fields"]["timetracking"]["originalEstimateSeconds"] == nil
-
-        #uncomment the two lines below to see a prettified version of the json
-        #puts "Here is prettified JSON data: "
-        #puts JSON.pretty_generate(data)
-        #
-        summarize_estimate(data)
-    else
-     raise StandardError, "Unsuccessful response code " + response.code + " for issue " + issue
+    if response.code !~ /20[0-9]{1}/
+      raise StandardError, "Unsuccessful response code #{response.code} for issue #{issue}"
     end
+    data = JSON.parse(response.body)
+    fields = data.keys
+
+    #puts JSON.pretty_generate(data)
+    print data["key"] + ","
+    print data["fields"]["reporter"]["displayName"] + ","
+    print data["fields"]["assignee"]["displayName"] + ","
+    print  data["fields"]["status"]["name"] + ","
+
+    data["fields"]["components"].each do |key|
+      print key["name"] + ";"
+    end
+
+    if data["fields"]["summary"].include? "QE "
+
+      ticket1 =  JiraTicket.new issue, data["fields"]["summary"],data["fields"]["created"],data["fields"]["status"]
+      @jiraTickets[issue] = ticket1
+      puts "======#{issue}#{data["fields"]["summary"]}#{data["fields"]["created"]}#{data["fields"]["status"]}"
+    end
+
+    data["fields"]["subtasks"].each do |key|
+      print key["key"] + ";"
+    end
+    print ","
+
+    print data["fields"]["created"] + ","
+
+    print data["fields"]["timetracking"]["originalEstimate"].to_s + "," unless data["fields"]["timetracking"]["originalEstimateSeconds"] == nil
+    print data["fields"]["timetracking"]["remainingEstimate"].to_s + "," unless data["fields"]["timetracking"]["originalEstimateSeconds"] == nil
+    #print data["fields"]["timetracking"]["timeSpent"] + ";"
+    print data["fields"]["timetracking"]["originalEstimateSeconds"].to_s + "," unless data["fields"]["timetracking"]["originalEstimateSeconds"] == nil
+    print data["fields"]["timetracking"]["remainingEstimateSeconds"].to_s + "," unless data["fields"]["timetracking"]["originalEstimateSeconds"] == nil
+    print data["fields"]["timetracking"]["timeSpentSeconds"].to_s + "," unless data["fields"]["timetracking"]["originalEstimateSeconds"] == nil
+
+    #uncomment the two lines below to see a prettified version of the json
+    #puts "Here is prettified JSON data: "
+    #puts JSON.pretty_generate(data)
+    #
+    summarize_estimate(data)
   end
 
   def summarize_estimate(ticket)
@@ -234,10 +228,12 @@ class JiraTicket
     @createdDate = createdDate
     @status = status
   end
+  attr_accessor :jiraId, :summary, :createdDate, :status
 end
 
 
 con = JiraConnection.new config_file: File.expand_path('../../jira-config.yml', __FILE__)
-pp con
-# con.pull_tickets
+#pp con
+#con.pull_tickets
 con.get_ticket "CD-28954"
+
