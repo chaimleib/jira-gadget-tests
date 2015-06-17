c5285e7f6591114946142ab206042e75035878f7
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-06-16 19:00:48 -0700

    non-working: add: scraper beginning

--- ruby-version/main.rb
+++ ruby-version/main.rb
@@ -3,6 +3,7 @@
 require 'rubygems'
 require 'nokogiri'
 require 'pp'
+require 'pry'
 require './secure_connection'
 
 class VersionScraper
@@ -12,8 +13,49 @@ class VersionScraper
 
   def scrape
     result = {}
-
+    
+    con = SecureConnection.new
+    html = con.submit_get @rel_uri
+    page = Nokogiri::HTML html
+    tables = get_tables page
+    puts "#{tables.length} tables extracted"
+    tables.each {|table|
+      scrape_table table, result
+    }
     result
+  end
+  
+  def get_table_headers(table)
+    first_row = table.at('tr')
+    headers = table.css('th') || table.css('td')
+    headers.map!{|th| th.text.strip }
+    headers
+  end
+    
+  def table_has_column(table, col)
+    get_table_headers(table).map{|col| col.downcase}.include? col
+  end
+  
+  def get_tables page
+    results = {}
+    tables = page.css('table[class=confluenceTable]')
+    binding.pry
+    # delete_if doesn't work on Nokogiri nodes
+    tables.delete_if{|table| !table_has_column(table, 'code freeze') }
+    table_headers = tables.map{|table| get_table_headers table }
+    results['headers'] = table_headers
+    results['data'] = []
+    tables.each{|table|
+      result_rows = []
+      results['data'].push result_rows
+      rows = table.css('tr')
+      rows.shift
+      rows.map{|row|
+        cells = row.css('td').map{|cell| cell.text.strip }
+        result_rows.push cells
+      }
+    }
+    results
   end
 end
 
